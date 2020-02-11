e4fe80bd716966cdbfcad88337fadfc39dd06e91
Ridez <lukaszlassota@gmail.com>
2020-02-11 19:43:21 +0100

    add Route for Single Setlist page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 
 import Home from '../pages/home'
+import Setlist from '../pages/_setlist'
 
 import '../styles/App.scss'
 
@@ -14,6 +15,11 @@ const App = () => {
           <Container>
             <Switch>
               <Route path="/" exact component={Home} />
+              <Route
+                path="/setlist/:artist/:venue/:id"
+                exact
+                component={Setlist}
+              />
             </Switch>
           </Container>
         </div>
