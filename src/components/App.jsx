d474aaed685d382c888d9996c960646163610627
Ridez <lukaszlassota@gmail.com>
2020-02-11 01:22:22 +0100

    Setup project

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import '../styles/App.scss'
+
+const App = () => {
+  return <div className="App">Display content</div>
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/firestore'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import {
+  createFirestoreInstance,
+  reduxFirestore,
+  getFirestore
+} from 'redux-firestore'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/index.scss'
+
+import App from './components/App'
+import reducers from './reducers'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+}
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+
+// Initialize firestore instance
+firebase.firestore()
+
+const store = createStore(
+  reducers,
+
+  // applyMiddleware(thunk.withExtraArgument({ getFirebase }))
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase, firebaseConfig),
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+}
+
+// eslint-disable-next-line no-undef
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  rootElement
+)
