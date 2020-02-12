26bd663047d51b9040bffa01d662cda556c1c6f7
Ridez <lukaszlassota@gmail.com>
2020-02-12 01:11:26 +0100

    add favs page, firestore setup and fontawesome icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { firestoreConnect } from 'react-redux-firebase'
 
-import Home from '../pages/home'
-import Setlist from '../pages/_setlist'
+import {
+  faVolumeDown,
+  faVolumeUp,
+  faVolumeMute
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faPlayCircle,
+  faPauseCircle
+} from '@fortawesome/free-regular-svg-icons'
+
 import Navbar from './Navbar'
+import Home from '../pages/home'
 import Favs from '../pages/favs'
 import Login from '../pages/login'
+import Setlist from '../pages/_setlist'
 
 import '../styles/App.scss'
+
+library.add(
+  fab,
+  faPlayCircle,
+  faPauseCircle,
+  faVolumeDown,
+  faVolumeUp,
+  faVolumeMute
+)
 
 const App = () => {
   return (
@@ -34,4 +59,23 @@ const App = () => {
   )
 }
 
-export default App
+export default compose(
+  connect(state => ({
+    auth: state.firebase.auth,
+    favsIds: state.firestore.data.favs
+  })),
+  firestoreConnect(({ auth }) => {
+    const queries = []
+
+    if (auth.uid != null) {
+      queries.push({
+        collection: 'users',
+        doc: auth.uid,
+        subcollections: [{ collection: 'setlists' }],
+        storeAs: 'favs'
+      })
+    }
+
+    return queries
+  })
+)(App)
