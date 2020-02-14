550efa37da092ca55dab725f83f2cb0e9e369348
Ridez <lukaszlassota@gmail.com>
2020-02-14 03:11:59 +0100

    remove unused actions

--- src/actions/favs.js
+++ src/actions/favs.js
@@ -1,16 +1,15 @@
-import setlists from '../apis/setlists'
-
 export const addToFavs = (setlistId, uid) => (
   dispatch,
   getState,
   { getFirestore }
 ) => {
   const firestore = getFirestore()
+
   firestore
     .collection('users')
     .doc(uid)
     .collection('setlists')
-    .doc(setlistId)
+    .doc(setlistId.id)
     .set({
       setlistId
     })
@@ -40,52 +39,4 @@ export const removeFromFavs = (setlistId, uid) => (
     .catch(() => {
       dispatch({ type: 'REMOVE_FROM_FAVS_ERROR', payload: true })
     })
-}
-
-export const setFavsLength = arrLength => {
-  return { type: 'SET_FAVS_LENGTH', payload: arrLength }
-}
-
-export const fetchSetlistsBySetlistId = setlistId => async dispatch => {
-  const response = setlistId.map(id =>
-    setlists.get('/setlistsBySetlistId', {
-      params: {
-        search: id.id
-      }
-    })
-  )
-  Promise.all(response).then(array => {
-    // Sort DESC array by date property
-    array.sort((a, b) => {
-      const aa = new Date(
-        a.data.eventDate
-          .split('-')
-          .reverse()
-          .join()
-      )
-      const bb = new Date(
-        b.data.eventDate
-          .split('-')
-          .reverse()
-          .join()
-      )
-
-      if (aa > bb) {
-        return -1
-      }
-      if (aa < bb) {
-        return 1
-      }
-      return 0
-    })
-
-    dispatch({
-      type: 'FETCH_SETLISTS_BY_SETLIST_ID',
-      payload: array
-    })
-  })
-}
-
-export const isLoadingFavs = val => {
-  return { type: 'IS_LOADING_FAVS', payload: val }
 }
